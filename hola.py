f84c38a4c18a800c251248050841d68a0adacf71
mszahan <mszahan1@gmail.com>
2025-05-16 20:41:05 +0600

    added fibonacci sequence generator

--- hola.py
+++ hola.py
@@ -4,3 +4,11 @@ for chd in word:
 
 ch_list = [ch for ch in word]
 print(ch_list)
+
+def fibonacci(n):
+    a, b = 0, 1
+    while a <=n:
+        yield a
+        a, b = b, a+b
+
+print(list(fibonacci(10)))
